a46ed6e55a80421ea0e73dad9fd1d7a9d03ff75c
Gravyon <rdjmartinez95@gmail.com>
2023-10-17 13:35:35 -0300

    task test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Passenger, { Flight, Reservation } from "./AirlineBooking";
 import BankAccount, { CheckingAccount, SavingsAccount } from "./BankAccount";
 import LibraryItem, { DVD } from "./LibraryItem";
 import Product from "./Product";
+import Task, { Project, User } from "./TaskManagement";
 
 const myBankAccount = new BankAccount();
 myBankAccount.deposit(122);
@@ -24,3 +25,11 @@ const date = new Date().getDate();
 const f1 = new Flight(22, "city", "tocity", date, 34);
 const r1 = new Reservation(p1, f1, 23);
 r1.getReservationInfo();
+
+const t1 = new Task(1, "crear", "creacion de asda");
+const u1 = new User(1, "user");
+const pro1 = new Project(1, "proyecto");
+t1.assignTo(u1);
+console.log(u1.getInfo());
+pro1.addTask(t1);
+console.log(pro1.getProjectInfo());
